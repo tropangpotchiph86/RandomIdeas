a6f3efabd95721ad8d78def71eaee87a012ace94
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-12 17:33:55 +0800

    02dotenv

--- routes/ideas.js
+++ routes/ideas.js
@@ -42,4 +42,50 @@ router.get('/:id', (req, res) => {
   res.json({ success: true, data: idea });
 });
 
+//Add an Idea
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+  // console.log(idea);
+  ideas.push(idea);
+  res.json({ success: true, data: idea });
+});
+
+//Update Idea
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({ success: true, data: idea });
+});
+
+//Delete Idea
+router.delete('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+
+  res.json({ success: true, data: {} });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const port = 5000;
 const app = express();
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //routes - root
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to RandomIdeas API' });
