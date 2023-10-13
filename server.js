567631579a9956c880e74b0acd63dab0441b46c1
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-13 10:21:31 +0800

    03backend

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const port = 5000;
 const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
 
 //Body Parser Middleware
 app.use(express.json());
