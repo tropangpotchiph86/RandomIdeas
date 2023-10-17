0d8121a4f5b7a06a4edce05f44b6be77c0399cec
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-17 09:43:57 +0800

    05femodal

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,5 @@
 import '@fortawesome/fontawesome-free/css/all.css';
+import Modal from './components/Modal';
 import './css/style.css';
 
-const modal = document.querySelector('#modal');
-const modalBtn = document.querySelector('#modal-btn');
-
-function open() {
-  modal.style.display = 'block';
-}
-
-function close() {
-  modal.style.display = 'none';
-}
-
-function outsideClick(e) {
-  if (e.target === modal) {
-    close();
-  }
-}
-
-modalBtn.addEventListener('click', open);
-window.addEventListener('click', outsideClick);
+const modal = new Modal();
